1e7044c9a586ac266d8595d335620dd796d8081d
kota_uchino <uchinokota@gmail.com>
2016-12-11 15:01:04 +0900

    housesテーブルにカラムを追加

---
+++ db/migrate/20161211052708_add_column_to_house.rb
@@ -0,0 +1,12 @@
+class AddColumnToHouse < ActiveRecord::Migration
+  def change
+    add_column :houses, :title, :string
+    add_column :houses, :description, :string
+    add_column :houses, :scale, :string
+    add_column :houses, :construction, :string
+    add_column :houses, :structure, :string
+    add_column :houses, :toilet, :string
+    add_column :houses, :price, :integer
+    add_column :houses, :note, :string
+  end
+end
