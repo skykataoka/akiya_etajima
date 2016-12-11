334c3736c933f0704a83f6d8ea0e85df4188ba9f
kota_uchino <uchinokota@gmail.com>
2016-12-11 15:56:39 +0900

    HouseモデルとArticleモデルのseedデータを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,31 @@
 #                status: status
 #   )
 # end
+
+# 20.times do
+#   address = Faker::Address.state + Faker::Address.city
+#   latitude = Faker::Address.latitude
+#   longitude = Faker::Address.longitude
+#   tel = Faker::PhoneNumber.cell_phone
+#   email = Faker::Internet.email
+#   title = Yoshida::Text.word
+#   description = Yoshida::Text.sentence
+#   House.create!(
+#                address: address,
+#                latitude: latitude,
+#                longitude: longitude,
+#                tel: tel,
+#                email: email,
+#                title: title,
+#                description: description
+#   )
+# end
+
+# 20.times do |i|
+#   house_id = i+1
+#   content = Yoshida::Text.sentences(10).join
+#   Article.create!(
+#     house_id: house_id,
+#     content: content
+#   )
+# end
