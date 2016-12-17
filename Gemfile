75d550c42c9e7d2aa31c438c8614d8e29b385a29
Susumu Kataoka <skykataoka@gmail.com>
2016-12-17 07:32:12 +0000

    ログイン機能追加。要検証

--- Gemfile
+++ Gemfile
@@ -45,5 +45,6 @@ group :development, :test do
   gem 'faker'
   gem 'pry-rails'
   gem 'yoshida'
+  gem 'devise'
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'top#index'
+  devise_for :users
   resources :houses
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
