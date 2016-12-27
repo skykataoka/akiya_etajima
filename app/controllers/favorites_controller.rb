e5bbc7f8d6ad17e54c150b6614a2bb351e30aebf
kota_uchino <uchinokota@gmail.com>
2016-12-27 17:53:01 +0900

    format.jsを追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,14 +2,26 @@ class FavoritesController < ApplicationController
   def create
     @house = House.find(params[:house_id])
     @favorite = Favorite.new(user_id: current_user.id, house_id: params[:house_id])
-    @favorite.save
-    redirect_to house_path(params[:house_id])
+    respond_to do |format|
+      binding.pry
+      if @favorite.save
+        format.js
+      else
+        format.html { redirect_to house_path(@house) }
+      end
+    end
   end
 
   def destroy
     @favorite = Favorite.find_by(user_id: current_user.id, house_id: params[:house_id])
-    @favorite.destroy
-    redirect_to house_path(params[:id])
+    respond_to do |format|
+      binding.pry
+      if @favorite.destroy
+        format.js
+      else
+        format.html { redirect_to house_path(@house) }
+      end
+    end
   end
 end
 
