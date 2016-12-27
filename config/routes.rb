732040b9f9521c99ad4da1f2bbecadb77f20c083
kota_uchino <uchinokota@gmail.com>
2016-12-27 17:24:51 +0900

    favoriteのroutingをhousesにネスト

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root 'top#index'
   devise_for :users
-  resources :houses
+  resources :houses do
+    resources :favorites , only: [:create, :destroy]
+  end
 
 
   # The priority is based upon order of creation: first created -> highest priority.
