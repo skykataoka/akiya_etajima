b9b15ddb565fbffba715d7bb7cf00a5829ca99f8
kota_uchino <uchinokota@gmail.com>
2016-12-27 17:23:05 +0900

    indexのviewでHouseデータの並び順をid順に変更

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -2,11 +2,12 @@ class HousesController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   
   def index
-    @houses = House.all
+    @houses = House.all.order('id')
   end
 
   def show
     @house = House.find(params[:id])
+    @favorite = Favorite.find_by(user_id: current_user.id, house_id: params[:id])
     @pin = Gmaps4rails.build_markers(@house) do |house, marker|
       marker.lat house.latitude
       marker.lng house.longitude
