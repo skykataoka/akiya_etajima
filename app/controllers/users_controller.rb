5ccd35b00d36773f390be92949da4d6a38bd93f3
kota_uchino <uchinokota@gmail.com>
2017-01-11 15:03:42 +0900

    users_controllerを作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+  def show
+    @favorite_houses =  @user.favorite_houses
+  end
+  
+  private
+  
+    def set_user
+      @user = User.find(current_user.id)
+    end
+
+end
