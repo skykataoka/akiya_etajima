51f17f4c556e6b28a32172d57f04f152564b0555
kota_uchino <uchinokota@gmail.com>
2017-01-13 13:59:18 +0900

    devise_parameter_sanitizerを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,5 +16,21 @@ class ApplicationController < ActionController::Base
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday,
+                                                         :avator,
+                                                         :address,
+                                                         :introduction,
+                                                         :want_to_do,
+                                                         :hobby,
+                                                         :skill,
+                                                         :occupation,
+                                                         :sex,
+                                                         :is_spouse,
+                                                         :child,
+                                                         :number_of_occupants,
+                                                         :budget_for_rent,
+                                                         :budget_for_buy,
+                                                         :status
+                                                        ])
     end
 end
