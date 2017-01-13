44d1eafc4f102acf291563689d42cd3d1678afab
kota_uchino <uchinokota@gmail.com>
2017-01-13 13:15:31 +0900

    deviceのconfigure_permitted_parametersにnameを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,5 +15,6 @@ class ApplicationController < ActionController::Base
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES)
       devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES)
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
     end
 end
