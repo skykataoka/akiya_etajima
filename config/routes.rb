c9ff5547a0cbee3e0b7359a959a326864890929c
kota_uchino <uchinokota@gmail.com>
2017-01-17 12:33:51 +0900

    routingの不具合を修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,7 @@ Rails.application.routes.draw do
   root 'top#index'
   
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
-  }
-
-  devise_for :users, controllers: {
+    registrations: 'users/registrations',
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   
