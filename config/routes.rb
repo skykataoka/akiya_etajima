c332947b6796a34c5489e26647c4bb448258a84c
kota_uchino <uchinokota@gmail.com>
2017-01-18 16:38:58 +0900

    messagesのルーティングをuser配下に修正

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,12 @@ Rails.application.routes.draw do
     omniauth_callbacks: "users/omniauth_callbacks"
   }
   
-  resource :user, only: [:show, :edit, :update]
+  resource :user, only: [:show, :edit, :update] do
+    resources :messages
+  end
   get :mypage, to: 'users#show'
   
   resources :houses do
     resources :favorites , only: [:create, :destroy]
-  end
-  
-  resources :conversations do
-    resources :messages
   end
 end
