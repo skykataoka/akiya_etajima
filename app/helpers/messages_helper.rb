bee6b57d83dc9c8cae174d9238f52f3f31ae6933
kota_uchino <uchinokota@gmail.com>
2017-01-18 18:59:45 +0900

    formをindex画面に設定 + formのパスをaction_nameによって分岐

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,2 +1,9 @@
 module MessagesHelper
+  def choose_new_or_edit
+    if action_name == 'index'
+      user_messages_path
+    else action_name == 'edit'
+      user_message_path
+    end
+  end
 end
