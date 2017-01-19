8299dbc7c19da378abddd7397e03197a19e6fc56
kota_uchino <uchinokota@gmail.com>
2017-01-19 13:10:29 +0900

    indexとcreateアクションを実装 + @current_admin_user_messagesにて表示するmessageを限定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,14 @@
 class MessagesController < ApplicationController
   def index
     @message = Message.new
+    @current_admin_user_messages = Message.where("(user_id = ?) OR (user_id = ?)", current_user.id, User.admin_user)
   end
 
   def create
+    @message = Message.new(messages_params)
+    @message.user_id = current_user.id
+    @message.save
+    redirect_to user_messages_path
   end
 
   def edit
@@ -13,5 +18,10 @@ class MessagesController < ApplicationController
   end
 
   def destroy
+  end
+  
+  private
+  def messages_params
+    params.require(:message).permit(:body)
   end
 end
