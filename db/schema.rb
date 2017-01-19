88223d942aef5937597382afcb11876b18435aa4
kota_uchino <uchinokota@gmail.com>
2017-01-19 16:02:41 +0900

    messages tableにconversation_idを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170119062935) do
+ActiveRecord::Schema.define(version: 20170119070000) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,9 +78,10 @@ ActiveRecord::Schema.define(version: 20170119062935) do
   create_table "messages", force: :cascade do |t|
     t.text     "body"
     t.integer  "user_id"
-    t.boolean  "read",       default: false
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.boolean  "read",            default: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.integer  "conversation_id"
   end
 
   add_index "messages", ["user_id"], name: "index_messages_on_user_id", using: :btree
