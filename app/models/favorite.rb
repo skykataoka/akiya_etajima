2cfbd0bc738bea5ddad8fead651e62132287d585
kota_uchino <uchinokota@gmail.com>
2017-01-21 15:28:58 +0900

    userがどのhouseに対してfavoriteしているのかを取得するクラスメソッドを実装

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,4 +1,8 @@
 class Favorite < ActiveRecord::Base
   belongs_to :house, counter_cache: :favorite_count
   belongs_to :user
+  
+  def self.get_favorite_info(user_id, house_id)
+    self.find_by(user_id: user_id, house_id: house_id)
+  end
 end
