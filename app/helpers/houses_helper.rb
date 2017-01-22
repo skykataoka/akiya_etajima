1406331aa42f3af5acfa2e14897176593fafe185
Susumu Kataoka <skykataoka@gmail.com>
2017-01-22 08:53:01 +0000

    ＣＳＶデータのインポート設定完了

--- app/helpers/houses_helper.rb
+++ app/helpers/houses_helper.rb
@@ -1,2 +1,12 @@
 module HousesHelper
+  def truncate(text, options = {}, &block)
+    if text
+      length  = options.fetch(:length, 200)
+      
+      content = text.truncate(length, options)
+      content = options[:escape] == false ? content.html_safe : ERB::Util.html_escape(content)
+      content << capture(&block) if block_given? && text.length > length
+      content
+    end
+  end
 end
