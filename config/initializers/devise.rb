26c3802decc1e7fcea5ae9ffe9af2bf64cd97740
Susumu Kataoka <skykataoka@gmail.com>
2017-01-23 06:13:01 +0000

    facebook環境設定修正

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,9 +272,12 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
 
-  if Rails.env.production?
-    config.omniauth :facebook, ENV["FACEBOOK_ID_PRODUCTION"], ENV["FACEBOOK_SECRET_PRODUCTION"], scope: 'user_birthday, user_about_me, email, public_profile', display: 'popup', info_fields: 'name, email'
-  else
-    config.omniauth :facebook, ENV["FACEBOOK_ID_DEVELOPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"], scope: 'user_birthday, user_about_me, email, public_profile', display: 'popup', info_fields: 'name, email, about, birthday'
+  Devise.setup do |config|
+    if Rails.env.production?
+      config.omniauth :facebook, ENV["FACEBOOK_ID_PRODUCTION"], ENV["FACEBOOK_SECRET_PRODUCTION"], scope: 'user_birthday, user_about_me, email, public_profile', display: 'popup', info_fields: 'name, email, about, birthday'
+    else
+      config.omniauth :facebook, ENV["FACEBOOK_ID_DEVELOPMENT"], ENV["FACEBOOK_SECRET_DEVELOPMENT"], scope: 'user_birthday, user_about_me, email, public_profile', display: 'popup', info_fields: 'name, email, about, birthday'
+    end
   end
+
 end
