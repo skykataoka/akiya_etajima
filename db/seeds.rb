92521cc5ebee3bf8e2ece9d09a81e125adc3f8c9
KataokaSusumu <skykataoka@gmail.com>
2017-01-27 21:46:32 +0900

    物件詳細項目整理

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 CSV.foreach('db/akiya_lists_for_demoday.csv') do |row|
   House.create(address: row[0],
-              expenses_status: row[1],
+              expenses_status: row[1],#  0 => "賃貸", 1 => "売買", 2 => "両方"
               price: row[2],
               note: row[3],
-              release_status: row[4], #  0 => "賃貸", 2 => "売買", 3 => "両方"
+              release_status: row[4],
               structure: row[5],
               scale: row[6],
               construction: row[7],
