74a3f114a638777849bb67eae132cbeeaee4c6f4
kota_uchino <uchinokota@gmail.com>
2017-01-29 01:53:56 +0900

    deviseのsessions controllerを継承するためにルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'top#index'
   
   devise_for :users, controllers: {
+    sessions: 'users/sessions',
     registrations: 'users/registrations',
     omniauth_callbacks: "users/omniauth_callbacks"
   }
